231374d866d7947e621ed5cf991033b768921d5f
ManganiBiagio <biagiomangani@live.it>
2022-12-06 17:00:15 +0100

    creazionee card

---
+++ src/components/CharacterCard.vue
@@ -0,0 +1,30 @@
+<template lang="">
+     <div class="card">
+          <div class="card-body">
+             <h5 class="card-title">{{character.name}}</h5>
+             <p class="card-text">
+                <ul>
+                    <li>height:{{character.height}}cm</li>
+                    <li>mass:{{character.mass}}kg</li>
+                    <li>hair color:{{character.hair_color}}</li>
+                    <li>skin color:{{character.skin_color}}</li>
+                    <li>eye color:{{character.eye_color}}</li>
+                    <li>birth year:{{character.birth_year}}</li>
+                    
+
+               </ul>
+            </p>
+          </div>
+     </div>
+</template>
+<script>
+export default {
+    props:{
+        character:Object
+    }
+
+}
+</script>
+<style lang="scss">
+
+</style>
--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -3,13 +3,7 @@
         <div class="title">Found {{store.count}} characters</div>
         <div class="row row-cols-1 row-cols-md-5 g-4 py-3">
             <div class="col" v-for="character in store.charactersList">
-                <div class="card">
-                     
-                         <div class="card-body">
-                             <h5 class="card-title">Card title</h5>
-                             <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                         </div>
-                </div>
+               <CharacterCard :character="character"></CharacterCard>
             </div>
         </div>
         
@@ -18,7 +12,9 @@
 </template>
 <script>
 import { store, fetchCharacters } from "../store";
+import CharacterCard from "./CharacterCard.vue"
 export default {
+    components:{CharacterCard},
     data() {
         return {
             store,
